33d3ae88a21e8719fb6010685f71d15ede0ffde2
=Talha Kumcu <=mkumcu151@gmail.com>
2023-12-03 18:18:50 +0100

    invoice updated

--- src/main/java/com/solvd/laba/block1/task2/Invoice.java
+++ src/main/java/com/solvd/laba/block1/task2/Invoice.java
@@ -2,7 +2,7 @@ package com.solvd.laba.block1.task2;
 
 
 public class Invoice {
-    private String serviceInfo;
+
     private String currency;
     private double amount;
 
@@ -22,14 +22,10 @@ public class Invoice {
         this.currency = currency;
     }
 
-    public void setServiceInfo(String serviceInfo) {
-        this.serviceInfo = serviceInfo;
-    }
 
     public void setInvoice(double cost, String address, String currency) {
         this.amount = cost;
         this.currency = currency;
-        this.serviceInfo = address;
     }
 
     public String getBill() {
